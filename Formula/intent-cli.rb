fc2120dbcce19b4bf17dd7e7be6b0473da45f536
Sergio Pulido <spulido8@gmail.com>
2025-10-18 13:39:45 +0200

    chore: update intent-cli v0.1.0 (darwin arm64)

--- Formula/intent-cli.rb
+++ Formula/intent-cli.rb
@@ -5,15 +5,20 @@ class IntentCli < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/intentregistry/intentregistry/releases/download/v0.1.0/intent-darwin-arm64.tar.gz"
-      sha256 "<sha256-arm64>"
+      url "https://github.com/intentregistry/intent-cli/releases/download/v0.1.0/intent-darwin-arm64.tar.gz"
+      sha256 "40083c553708bb0a30fa639878d6c577eb70b77d90c7d2f7becc301d52bb2ea0"
     else
-      url "https://github.com/intentregistry/intentregistry/releases/download/v0.1.0/intent-darwin-x64.tar.gz"
-      sha256 "<sha256-x64>"
+      # url "https://github.com/intentregistry/intent-cli/releases/download/v0.1.0/intent-darwin-x64.tar.gz"
+      # sha256 "<sha256-x64>"
+      odie "Intel-based builds are not available yet"
     end
   end
 
   def install
     bin.install "intent"
+  end
+
+  test do
+    system "#{bin}/intent", "--help"
   end
 end
